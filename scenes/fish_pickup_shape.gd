f915c51250fcc7ffc2178750d53fe36e8d06b44f
Squiller34 <alexis.gobaille@gmail.com>
2026-09-13 11:19:08 +0200

    added foods

---
+++ scenes/fish_pickup_shape.gd
@@ -0,0 +1,36 @@
+extends CollisionShape2D
+
+var chef_in_area;
+@export var chef:Chef
+@export var fish_pickup_action:Sprite2D
+@export var fish_in_hand:Sprite2D
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	chef_in_area = false
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	fish_pickup_action.visible = false
+	
+	if chef_in_area and Input.is_action_just_pressed("chef_action"):
+		if not chef.has_fish:
+			chef.pickup_fish()
+		else:
+			chef.put_fish_back()
+		
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.get_name() == "chef":
+		chef_in_area = true
+		if not chef.has_fish:
+			fish_pickup_action.visible = true;
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body.get_name() == "chef":
+		chef_in_area = false
+		fish_pickup_action.visible = false;
--- scripts/chef.gd
+++ scripts/chef.gd
@@ -1,11 +1,29 @@
 extends CharacterBody2D
 
+class_name Chef
 
 const speed = 600
+
+var has_fish = false
+
+func give_fish() -> void:
+	has_fish = false
+	$FishInHand.visible = false
+	
+func pickup_fish() -> void:
+	has_fish = true
+	$FishInHand.visible = true
+	
+func put_fish_back() -> void:
+	give_fish()
+	
+	
 
 func get_input():
 	var input_direction = Input.get_vector("chef_left", "chef_right", "chef_up", "chef_down")
 	velocity = input_direction * speed
+	print(has_fish)
+	$FishInHand.visible = false
 	if(input_direction[0] < 0):
 		$ChefTextures.play("left_walking")
 	elif(input_direction[0] > 0):
